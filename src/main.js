b6b99486e63df63f851157d42fdbd0be5de780e2
Sarthak Rakhra <sr691312@dal.ca>
2021-04-20 15:34:31 -0300

    configured project to use third party design libraries

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Buefy from "buefy";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import "buefy/dist/buefy.css";
+
+library.add(fas);
+
+Vue.component("faIcon", FontAwesomeIcon);
+Vue.use(Buefy, {
+  defaultIconComponent: FontAwesomeIcon,
+  defaultIconPack: "fas",
+});
 
 Vue.config.productionTip = false;
 
