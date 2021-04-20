6b105314f0705121926f82ef5a7b4a42e64ec775
Sarthak Rakhra <sr691312@dal.ca>
2021-04-20 17:45:00 -0300

    changing scope of text alignment for box in homepage

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,3 @@
     <router-view />
   </div>
 </template>
-<style lang="scss" scoped>
-#app {
-  text-align: center;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="columns">
       <div class="column is-half is-offset-one-quarter">
-        <div class="box">
+        <div class="box has-text-centered">
           <h1 class="title">Please select an action</h1>
           <b-button
             to="/dataSubmission"
