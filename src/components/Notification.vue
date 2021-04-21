df2f7e00d75a8deb06db437b4dd019968cf72f10
Sarthak Rakhra <sr691312@dal.ca>
2021-04-21 19:50:09 -0300

    creating a notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-notification
+    v-if="showNotification"
+    :type="type"
+    has-icon
+    aria-close-label="Close notification"
+    auto-close
+    :duration="8500"
+    :closable="false"
+  >
+    {{ message }}
+  </b-notification>
+</template>
+<script>
+export default {
+  name: "Notification",
+  props: {
+    type: String,
+    message: String,
+    showNotification: Boolean,
+  },
+};
+</script>
